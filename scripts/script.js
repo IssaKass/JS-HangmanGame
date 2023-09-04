f9424a3cd4b204a2a5cbb7e43970d2eeca7a3680
Abdallah-Issa Kass <issakass02@gmail.com>
2023-09-04 23:06:32 +0300

    Upload the design

---
+++ scripts/script.js
@@ -0,0 +1,100 @@
+const gameModal = document.querySelector(".game-modal");
+const gameModalImg = document.querySelector(".game-modal__image");
+const gameModalTitle = document.querySelector(".game-modal__title");
+const gameModalMessage = document.querySelector(".game-modal__message");
+const playAgainButton = document.getElementById("playAgainButton");
+const hangmanImage = document.getElementById("hangmanImage");
+const wordToGuess = document.getElementById("wordToGuess");
+const hintContent = document.getElementById("hintContent");
+const incorrectGuesses = document.getElementById("incorrectGuesses");
+const keyboard = document.getElementById("keyboard");
+
+let currentWord = "";
+let wrongGuessCount = 0;
+let correctLetters = [];
+const maxGuesses = 5;
+
+function startGame() {
+  reset();
+  hideModal();
+  createKeyboard();
+
+  hangmanImage.src = "../images/hangman-0.svg";
+  const { word, hint } = getRandom(wordlist);
+  currentWord = word;
+
+  wordToGuess.innerHTML = word
+    .split("")
+    .map(() => `<li class = "word-to-guess__letter"></li>`)
+    .join("");
+  hintContent.textContent = hint;
+}
+
+function reset() {
+  wrongGuessCount = 0;
+  correctLetters = [];
+
+  gameModal.classList.add("game-modal--hidden");
+  incorrectGuesses.textContent = `${wrongGuessCount} / ${maxGuesses}`;
+}
+
+function hideModal() {
+  gameModal.classList.remove("show");
+}
+
+function createKeyboard() {
+  keyboard.innerHTML = "";
+  for (let i = 97; i < 123; i++) {
+    const button = document.createElement("button");
+    button.classList.add("key");
+    button.textContent = String.fromCharCode(i);
+    button.addEventListener("click", (e) => {
+      initGame(e.target, String.fromCharCode(i));
+    });
+    keyboard.appendChild(button);
+  }
+}
+
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function initGame(button, clickedLetter) {
+  if (currentWord.includes(clickedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+      if (letter === clickedLetter) {
+        correctLetters.push(letter);
+        wordToGuess.querySelectorAll("li")[index].innerText = letter;
+      }
+    });
+  } else {
+    wrongGuessCount++;
+    hangmanImage.src = `../images/hangman-${wrongGuessCount}.svg`;
+  }
+  button.disabled = true;
+  incorrectGuesses.textContent = `${wrongGuessCount} / ${maxGuesses}`;
+
+  if (wrongGuessCount === maxGuesses) {
+    return gameOver(false);
+  }
+  if (correctLetters.length === currentWord.length) {
+    return gameOver(true);
+  }
+}
+
+const gameOver = (isVictory) => {
+  setTimeout(() => {
+    gameModal.classList.remove("game-modal--hidden");
+    gameModalImg.src = `../images/${isVictory ? "victory" : "lost"}.gif`;
+    gameModalTitle.textContent = isVictory ? "Congrats!" : "Game Over!";
+    gameModalMessage.innerHTML = `${
+      isVictory ? "You found the word" : "The correct word was"
+    }: <b>${currentWord}</b>`;
+  }, 500);
+};
+
+playAgainButton.addEventListener("click", () => {
+  startGame();
+});
+
+startGame();
---
+++ scripts/wordlist.js
@@ -0,0 +1,54 @@
+const wordlist = [
+  {
+    word: "guitar",
+    hint: "A musical instrument with strings.",
+  },
+  {
+    word: "oxygen",
+    hint: "A colorless, odorless gas essential for life.",
+  },
+  {
+    word: "painting",
+    hint: "An art form using colors on a surface to create images or expression.",
+  },
+  {
+    word: "astronomy",
+    hint: "The scientific study of celestial objects and phenomena.",
+  },
+  {
+    word: "bicycle",
+    hint: "A two-wheeled vehicle powered by pedals.",
+  },
+  {
+    word: "computer",
+    hint: "An electronic device that processes data and performs tasks.",
+  },
+  {
+    word: "ocean",
+    hint: "A vast body of saltwater covering most of the Earth's surface.",
+  },
+  {
+    word: "book",
+    hint: "A written or printed work consisting of pages bound together.",
+  },
+  {
+    word: "mountain",
+    hint: "A large landform that rises prominently above its surroundings.",
+  },
+  {
+    word: "dolphin",
+    hint: "A highly intelligent marine mammal known for its playful behavior.",
+  },
+  {
+    word: "camera",
+    hint: "A device used to capture photographs or record videos.",
+  },
+  {
+    word: "butterfly",
+    hint: "An insect with colorful wings that undergoes metamorphosis.",
+  },
+  {
+    word: "history",
+    hint: "The study of past events and their impact on society.",
+  },
+];
