6d9f191ee7bbcea131a09cdc1bc108ef80ed5c82
Abdallah Issa Kass <issakass02@gmail.com>
2023-09-05 10:33:19 +0300

    Update wordlist.js

--- scripts/wordlist.js
+++ scripts/wordlist.js
@@ -51,4 +51,60 @@ const wordlist = [
     word: "history",
     hint: "The study of past events and their impact on society.",
   },
+  {
+  word: "jungle",
+  hint: "A dense and tropical forest with abundant vegetation."
+  },
+  {
+    word: "piano",
+    hint: "A musical instrument with keys that produce sound when struck."
+  },
+  {
+    word: "planet",
+    hint: "A celestial body that orbits a star, such as Earth or Mars."
+  },
+  {
+    word: "science",
+    hint: "The systematic study of the natural world and its phenomena."
+  },
+  {
+  word: "forest",
+  hint: "A large area covered chiefly with trees and undergrowth."
+  },
+  {
+    word: "telephone",
+    hint: "A communication device used for voice conversations."
+  },
+  {
+    word: "chemistry",
+    hint: "The branch of science that deals with the composition of substances."
+  },
+  {
+    word: "basketball",
+    hint: "A team sport played with a round ball and a hoop."
+  },
+  {
+    word: "sunflower",
+    hint: "A tall, yellow-flowered plant that follows the sun's movement."
+  },
+  {
+    word: "architecture",
+    hint: "The art and science of designing and constructing buildings."
+  },
+  {
+    word: "volcano",
+    hint: "A mountain that erupts with molten rock, ash, and gases."
+  },
+  {
+    word: "dinosaur",
+    hint: "A prehistoric reptile that lived millions of years ago."
+  },
+  {
+    word: "festival",
+    hint: "A celebration or event marked by joyous activities."
+  },
+  {
+    word: "galaxy",
+    hint: "A massive system of stars, gas, dust, and dark matter."
+  }
 ];
